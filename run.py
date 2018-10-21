629513cd527bfad102bbf77563a9d8489c4f8d26
Paul-weqe <paul1tw1@gmail.com>
2018-10-21 15:07:47 +0300

    ...

--- run.py
+++ run.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "2018"
 # SECRET_KEY = '2018'
 
+
+
 counties_info = []
 for x in counties_waterpoints:
     counties_info.append({"name":x, "population": counties_waterpoints[x]})
+
+
 
 @app.route("/")
 def index():
@@ -25,24 +29,32 @@ def index():
         number_of_wards=len(all_wards), searchForm=searchForm)
 
 
-@app.route("/waterpoint-info/<int:waterpoint_id>")
+
+@app.route("/waterpoint-info/<waterpoint_id>")
 def waterpoint_info(waterpoint_id):
     waterpoint = None
+    searchForm = SearchForm()
     for point in all_waterpoints:
         if point["id"] == int(waterpoint_id):
             waterpoint = point
             break
     if waterpoint == None:
         return render_template("waterpoint_not_found.html")
-    return render_template("waterpoint_info.html", waterpoint_data=waterpoint)
+    return render_template("waterpoint_info.html", waterpoint_data=waterpoint, searchForm=searchForm)
+
+
 
 @app.route("/basic-tables")
 def basic_tables():
     return render_template("tables-basic.html")
 
+
+
 @app.route('/data-tables')
 def data_tables():
     return render_template("tables-data.html")
+
+
 
 @app.route('/google-maps')
 def google_maps():
@@ -52,6 +64,8 @@ def google_maps():
     # all_waterpoint_locations = all_waterpoint_locations
     return render_template("maps-gmap.html", central_point_json=central_point_json,
             all_waterpoint_locations=all_waterpoint_locations, searchForm=searchForm)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
